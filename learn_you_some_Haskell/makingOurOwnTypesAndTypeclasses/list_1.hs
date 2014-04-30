c03e59e25dd9d9be6c1bd8c6ffec6900e918a21f
DeadDork <nimrodomer@gmail.com>
2014-04-30 14:48:41 -0700

    Demo the list type

--- learn_you_some_Haskell/makingOurOwnTypesAndTypeclasses/list_1.hs
+++ learn_you_some_Haskell/makingOurOwnTypesAndTypeclasses/list_1.hs
@@ -1,1 +1,13 @@
-data List a = Empty | ListCons a (List a) deriving (Show, Read, Eq, Ord)
+data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+
+-- How about that:
+-- ghci> Empty
+-- Empty
+-- ghci> 5 `C
+-- Char  Cons
+-- ghci> 5 `Cons` Empty
+-- Cons 5 Empty
+-- ghci> 4 `Cons` (5 `Cons` Empty)
+-- Cons 4 (Cons 5 Empty)
+-- ghci> 3 `Cons` (4 `Cons` (5 `Cons` Empty))
+-- Cons 3 (Cons 4 (Cons 5 Empty))
