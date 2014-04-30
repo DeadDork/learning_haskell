9299730197af307794f59ce311e62fade8351892
DeadDork <nimrodomer@gmail.com>
2014-04-30 00:03:21 -0700

    Oops!

--- learn_you_some_Haskell/makingOurOwnTypesAndTypeclasses/vector_1.hs
+++ learn_you_some_Haskell/makingOurOwnTypesAndTypeclasses/vector_1.hs
@@ -6,5 +6,5 @@ vplus :: (Num a) => Vector a -> Vector a -> Vector a
 vectMult :: (Num a) => Vector a -> a -> Vector a
 (Vector a b c) `vectMult` m = Vector (a*m) (b*m) (c*m)
 
-scalarMult :: (Num a) => Vector a -> Vector a -> Vector a
-(Vector a b c) `scalarMult` (Vector z y x) = Vector (a*z) (b*y) (c*x)
+scalarMult :: (Num a) => Vector a -> Vector a -> a
+(Vector a b c) `scalarMult` (Vector z y x) = a*z + b*y + c*x
