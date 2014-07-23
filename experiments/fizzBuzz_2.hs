a6c660d7f2b793bd4e90e4602319ee77ddfb4913
DeadDork <nimrodomer@gmail.com>
2014-07-22 22:16:47 -0700

    cleaned up

--- experiments/fizzBuzz_2.hs
+++ experiments/fizzBuzz_2.hs
@@ -1,6 +1,9 @@
-fizzBuzz :: (Integral a, Show a) => [a] -> [[Char]]
+-- Fizz Buzz with list comprehension
+
+fizzBuzz :: (Integral a, Show a) => [a] -> [String]
 fizzBuzz xs = [fB x | x <- xs]
-fB :: (Integral a, Show a) => a -> [Char]
+
+fB :: (Integral a, Show a) => a -> String
 fB x
     | x `mod` 15 == 0 = "Fizz Buzz"
     | x `mod` 3 == 0 = "Fizz"
