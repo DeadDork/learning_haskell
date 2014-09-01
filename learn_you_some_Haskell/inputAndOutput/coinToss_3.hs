fab01df43e89b6e45f6d444ba9faf0714d82c671
DeadDork <nimrodomer@gmail.com>
2014-08-31 22:06:53 -0700

    Added a coin toss function (used it to better understand fmap on IO)

--- learn_you_some_Haskell/inputAndOutput/coinToss_3.hs
+++ learn_you_some_Haskell/inputAndOutput/coinToss_3.hs
@@ -10,3 +10,8 @@ instance Random Coin where
     randomR (a, b) g = case randomR (fromEnum a, fromEnum b) g of
         (x, g') -> (toEnum x, g')
     random g = randomR (minBound, maxBound) g
+
+flip' :: IO Coin
+flip' = do g <- newStdGen
+           let (v, _) = random g :: (Coin, StdGen)
+           return v
