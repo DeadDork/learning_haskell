0243db1790f1509a53f7eaa39e0f4fcacc6414be
DeadDork <nimrodomer@gmail.com>
2014-09-22 17:53:39 -0700

    Corrected prime sieve, per M. O'Neill's paper

--- ProjectEuler/solutions/Primes.hs
+++ ProjectEuler/solutions/Primes.hs
@@ -1,34 +1,35 @@
 module Primes
-( prime
+( isPrime
 , primes
 , factors
 , Natural
 ) where
 
+import Data.Map.Lazy as Map
 import Data.Natural
 
-prime :: Natural -> Bool
-prime 0 = False
-prime 1 = False
-prime n = prime' primes n
+isPrime :: Natural -> Bool
+isPrime n =
+    let isPrime' _ [] = False
+        isPrime' n (x:xs) | n == x    = True
+                          | n < x     = False
+                          | otherwise = isPrime' n xs
+    in isPrime' n primes
 
-prime' :: [Natural] -> Natural -> Bool
-prime' (x:xs) y | x == y    = True
-                | x < y     = prime' xs y
-                | otherwise = False
- 
 primes :: [Natural]
-primes = sieve (\ x y -> y `mod` x /= 0) [2..]
- 
-sieve :: (Natural -> Natural -> Bool) -> [Natural] -> [Natural]
-sieve _ [] = []
-sieve f (x:xs) = x : sieve f (filter (f x) xs)
+primes =
+    let sieve xs = sieve' xs Map.empty
+        sieve' [] _ = []
+        sieve' (x:xs) table = case Map.lookup x table of
+            Nothing      -> x : sieve' xs (Map.insert (x^2) [x] table)
+            Just factors -> sieve' xs (Prelude.foldl (reinsert x) (Map.delete x table) factors)
+        reinsert x table p = Map.insertWith (++) (x + p) [p] table
+    in sieve [2..]
 
 factors :: Natural -> [Natural]
 factors 0 = error "Every number is a factor of zero (e.g. 5 x 0 = 0)"
-factors n = factors' n primes
-
-factors' :: Natural -> [Natural] -> [Natural]
-factors' 1 _ = [1]
-factors' n (x:xs) | n `mod` x == 0 = x : factors' (n `div` x) (x:xs)
-                  | otherwise      = factors' n xs
+factors n =
+    let factors' 1 _ = [1]
+        factors' n (x:xs) | n `mod` x == 0 = x : factors' (n `div` x) (x:xs)
+                          | otherwise      = factors' n xs
+    in factors' n primes
