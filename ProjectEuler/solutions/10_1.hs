d2ff4691a626ea3e218ae803f7869ca51e426f43
DeadDork <nimrodomer@gmail.com>
2014-09-22 17:54:41 -0700

    Works, now that the prime sieve has been corrected

--- ProjectEuler/solutions/10_1.hs
+++ ProjectEuler/solutions/10_1.hs
@@ -1,15 +1,7 @@
--- Too slow.
-
--- The Sieve of Erastosthenes uses up too much memory.
-
 import Primes
 
 main :: IO ()
-main = print $ f 2000000
+main = print $ primeSumBelow 2000000
 
-f :: Natural -> Natural
-f n = f' primes n
-
-f' :: [Natural] -> Natural -> Natural
-f' (x:xs) n | x > n = 0
-            | otherwise = x + (f' xs n)
+primeSumBelow :: Natural -> Natural
+primeSumBelow n = sum $ takeWhile (< n) primes
