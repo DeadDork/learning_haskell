01d61b84342879e5f66bc62aabe9a1edc11cdc03
Nimrod Omer <https://github.com/DeadDork>
2014-12-24 13:39:28 -0800

    Init: implement a stack

---
+++ purely_functional_data_structures/chapter_two/stack_1.hs
@@ -0,0 +1,32 @@
+module Stack
+( Stack (..)
+, empty
+, isEmpty
+, cons
+, head
+, tail
+) where
+
+  import Prelude hiding (head, tail)
+
+  data Stack a = Empty
+               | Cons a (Stack a)
+               deriving (Show, Read, Eq, Ord)
+
+  cons :: a -> Stack a -> Stack a
+  cons x xs = x `Cons` xs
+
+  empty :: Stack a
+  empty = Empty
+
+  isEmpty :: Stack a -> Bool
+  isEmpty Empty = True
+  isEmpty _ = False
+
+  head :: Stack a -> Maybe a
+  head Empty = Nothing
+  head (x `Cons` xs) = Just x
+
+  tail :: Stack a -> Maybe (Stack a)
+  tail Empty = Nothing
+  tail (x `Cons` xs) = Just xs
