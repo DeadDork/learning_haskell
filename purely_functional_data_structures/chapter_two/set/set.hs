06f6302e3110db92050dcfdc7e3aecc947e731e2
Nimrod Omer <https://github.com/DeadDork>
2014-12-26 00:41:44 -0800

    Added a couple of fun algorithms

--- purely_functional_data_structures/chapter_two/set/set.hs
+++ purely_functional_data_structures/chapter_two/set/set.hs
@@ -1,6 +1,7 @@
 module Set
 ( Set
 , empty
+, fromList
 , insert
 , member
 ) where
@@ -12,16 +13,40 @@ module Set
   empty :: Set a
   empty = Empty
 
-  member :: (Ord a) => Set a -> a -> Bool
-  member Empty _ = False
-  member (Node x left right) y | y == x = True
-                               | y < x = member left y
-                               | otherwise = member right y
+  member :: (Ord a) => a -> Set a -> Bool
+  member _ Empty = False
+  member x (Node y left right)
+     | x == y = True
+     | x < y = member x left
+     | x > y = member x right
 
-  insert :: (Ord a) => Set a -> a -> Set a
-  insert Empty y = Node y Empty Empty
-  insert (Node x left right) y | x == y = Node x left right
-                               | x > y = insert left y
-                               | x < y = insert right y
+  insert :: (Ord a) => a -> Set a -> Set a
+  insert x Empty = Node x Empty Empty
+  insert x (Node y left right)
+     | x == y = Node y left right
+     | x > y = Node y left (insert x right)
+     | x < y = Node y (insert x left) right
 
-  fromList :: [a] -> Set a
+  fromList :: (Ord a) => [a] -> Set a
+  fromList = foldr insert Empty
+
+  {- Naive implementation.
+  -- N.B. very slow because of how ++ catenates in Haskell.
+  toList :: (Ord a) => Set a -> [a]
+  toList Empty = []
+  toList (Node x left right)
+    | left == Empty && right == Empty = [x]
+    | otherwise = (toList left) ++ [x] ++ (toList right)
+  -}
+
+  -- Utilizes treeFoldr on cons, so cons efficiently from right to left.
+  -- N.B. Given the path treeFoldr takes, the output list is sorted.
+  toList :: (Ord a) => Set a -> [a]
+  toList = treeFoldr (:) []
+
+  treeFoldr :: (Ord a) => (a -> b -> b) -> b -> Set a -> b
+  treeFoldr _ a Empty = a
+  treeFoldr f a (Node x Empty Empty) = f x a
+  treeFoldr f a (Node x Empty right) = f x $ treeFoldr f a right
+  treeFoldr f a (Node x left right) = treeFoldr f (f x $ treeFoldr f a right) left
+  treeFoldr f a (Node x left Empty) = f x $ treeFoldr f a left
