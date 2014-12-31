31176dfb4c3b361d06b4332e9960b7577bdd6340
Nimrod Omer <nimrodomer@gmail.com>
2014-12-30 18:49:57 -0800

    Heapsort, bitches!

--- experiments/heapSort_1.hs
+++ experiments/heapSort_1.hs
@@ -1,9 +1,12 @@
 module Sort
 ( fromList
--- , heapSort -- Currently making my head explode (and not in a good way)
--- , insert -- Hard to do this without an array to index
-, lengthH
+, heapSort
+, insert
+, Sort.length
+, pop
+, singleton
 , toList
+, update
 ) where
 
   import qualified Data.Vector.Unboxed as U
@@ -22,14 +25,12 @@ module Sort
   fromList :: (Ord a, U.Unbox a) => [a] -> Heap a
   fromList [] = Empty
   fromList xs = fromList' 0 $ U.fromList xs
-
-  fromList' :: (Ord a, U.Unbox a) => Index -> U.Vector a -> Heap a
-  fromList' ip vx = heapify $
-    case vx U.!? ip of
-         Nothing -> Empty
-         Just vp -> Node ip vp (fromList' il vx) (fromList' ir vx)
-           where il = 2 * ip + 1
-                 ir = 2 * ip + 2
+    where fromList' ip vx = heapify $
+            case vx U.!? ip of
+                 Nothing -> Empty
+                 Just vp -> Node ip vp (fromList' il vx) (fromList' ir vx)
+                   where il = 2 * ip + 1
+                         ir = 2 * ip + 2
 
   heapify :: (Ord a) => Heap a -> Heap a
   heapify Empty = Empty
@@ -39,12 +40,13 @@ module Sort
     | otherwise = Node i v (heapify l) (heapify r)
       where leftSwap (Node ip vp (Node il vl ll rl) r) = Node ip vl (Node il vp ll rl) r
             rightSwap (Node ip vp l (Node ir vr lr rr)) = Node ip vr l (Node ir vp lr rr)
-            -- N.B. These pattern matches don't need to be exhaustive, given
-            -- the guards above.
+            -- N.B. These pattern matches don't need to be exhaustive, as
+            -- combined with the guards above, you have exhaustive pattern
+            -- matching.
 
   toList :: (Ord a, U.Unbox a) => Heap a -> [a]
   toList h = U.toList $ U.create $ do
-    vec <- M.new $ lengthH h
+    vec <- M.new $ Sort.length h
     massWrite h vec
     return vec
       where massWrite Empty _ = return ()
@@ -53,10 +55,48 @@ module Sort
               massWrite l vec
               massWrite r vec
 
-  -- Fastest implementation I could think of. It's O (n)
-  -- (branching down to the Empties is O (log n), but the real cost is in
-  -- adding n 1's. It's faster than finding the largest Index, though.
-  lengthH :: Heap a -> Int
-  lengthH h = f h - 1
-    where f Empty = 1
-          f (Node _ _ l r) = f l + f r
+  -- Fastest implementation I could think of. It's O (n) (branching down to the
+  -- Empties is a cheap (n + 1) recursions, as is adding (n + 1) 1's). An
+  -- alternative is to compare n log (n) Indexes, but the thunks are more
+  -- expensive than the below.
+  length :: Heap a -> Int
+  length h = length' h - 1
+    where length' Empty = 1
+          length' (Node _ _ l r) = length' l + length' r
+
+  update :: (Ord a) => Index -> a -> Heap a -> Heap a
+  update _ _ Empty = Empty
+  update i v h@(Node j w l r)
+    | i == j = Node i v l r
+    | i > j = heapify $ Node j w (update i v l) (update i v r)
+    | i < j = h
+
+  insert ::(Ord a) => a -> Heap a -> Heap a
+  insert v Empty = singleton v
+  insert v h = insert' (Sort.length h) v h
+    where insert' i v Empty = Empty
+          insert' i v (Node j w l r)
+            | i == 2 * j + 1 = heapify $ Node j w (Node i v Empty Empty) r
+            | i == 2 * j + 2 = heapify $ Node j w l (Node i v Empty Empty)
+            | otherwise = heapify $ Node j w (insert' i v l) (insert' i v r)
+
+  singleton :: (Ord a) => a -> Heap a
+  singleton v = Node 0 v Empty Empty
+
+  pop :: (Ord a) => Heap a -> (a, Heap a)
+  pop Empty = error "Empty heap"
+  pop p@(Node _ v _ _) = (v, downShift p)
+    where downShift (Node i _ l r)
+            | l <= r =
+                case r of
+                     Node _ vr _ _ -> Node i vr l (downShift r)
+                     Empty -> Empty -- i.e. both l & r = Empty
+            | otherwise =
+                case l of
+                     Node _ vl _ _ -> Node i vl (downShift l) r
+
+  heapSort :: (Ord a) => Heap a -> [a]
+  heapSort Empty = []
+  heapSort h =
+    let (v,h') = pop h
+     in v : heapSort h'
