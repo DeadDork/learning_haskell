3be29db872cd3a99e4dbf8257872d19dfe442d6c
Nimrod Omer <nimrodomer@gmail.com>
2015-06-01 10:47:35 -0500

    Refactored

--- experiments/bottlesOfBeer_2.hs
+++ experiments/bottlesOfBeer_2.hs
@@ -1,22 +1,20 @@
 module Main where
   import Control.Monad.Writer
 
+  main :: IO ()
+  main = putStr songString
+
   songString :: String
   songString =
     let
-      sing = tell
+      verse n = do
+        tell $ n `bottlesOfBeer` " on the wall.\n"
+        tell $ n `bottlesOfBeer` ".\n"
+        tell "Take one down, pass it around.\n"
+        tell $ (n - 1) `bottlesOfBeer` " on the wall.\n\n"
+      bottlesOfBeer n = (someBottles n ++)
       someBottles n
         | n == 0     = "No more bottles of beer"
         | n == 1     = "1 bottle of beer"
         | otherwise = show n ++ " bottles of beer"
-      bottlesOfBeer n = (someBottles n ++)
-      verse n = do
-        sing $ n `bottlesOfBeer` " on the wall.\n"
-        sing $ n `bottlesOfBeer` ".\n"
-        sing "Take one down, pass it around.\n"
-        sing $ (n - 1) `bottlesOfBeer` " on the wall.\n"
-      song = execWriter $ mapM_ verse [100, 99 .. 1]
-    in song
-
-  main :: IO ()
-  main = putStr songString
+    in execWriter $ mapM_ verse [100, 99 .. 1]
