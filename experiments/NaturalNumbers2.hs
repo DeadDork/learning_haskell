f96ceb9bd56efe72a454c31de0a8e15a3471f804
Nimrod Omer <nimrodomer@gmail.com>
2015-06-30 23:52:19 -0500

    Init: a simple natural number type

---
+++ experiments/NaturalNumbers2.hs
@@ -0,0 +1,49 @@
+module NaturalNumbers2
+  ( Natural
+  ) where
+
+  data Natural = One
+               | Succ Natural deriving (Eq, Ord)
+
+  instance Integral Natural where
+    m `quotRem` n = (quotN, remN)
+      where quotN = fromInteger $ toInteger m `quot` toInteger n
+            remN = fromInteger $ toInteger m `rem` toInteger n
+    toInteger = toInteger . (1 +) . fromEnum
+
+  instance Enum Natural where
+    fromEnum = fromEnum . toInteger
+    toEnum = fromInteger . toInteger
+
+  instance Real Natural where
+    toRational = fromInteger . toInteger
+
+  instance Num Natural where
+    m + n
+      | n == One = Succ m
+      | otherwise = Succ m + (n - One)
+    m * n
+      | n == One = m
+      | otherwise = m + (m * (n - One))
+    m - n
+      | m == One = naturalError
+      | n == One = f m
+      | otherwise = m - (n - One)
+        where f (Succ l) = l
+              f _ = One
+    abs n = n
+    signum _ = 1
+    fromInteger n
+      | n < 1 = naturalError
+      | n == 1 = One
+      | otherwise = Succ (fromInteger (n - 1))
+    negate _ = naturalError
+
+  instance Show Natural where
+    show n
+      | n == One = "One"
+      | n == Succ One = "Succ One"
+      | otherwise = "Succ (" ++ show (n - 1) ++ ")"
+
+  naturalError :: a
+  naturalError = error "A natural number is here defined as an integer greater than 1."
