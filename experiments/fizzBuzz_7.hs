d7d172e27087a8abd4289268d8ba056fcfb0644c
Nimrod Omer <nimrodomer@gmail.com>
2015-09-01 04:06:59 -0700

    Init: a working fB_6

---
+++ experiments/fizzBuzz_7.hs
@@ -0,0 +1,10 @@
+module FizzBuzz
+  ( fizzBuzz
+  ) where
+
+  fizzBuzz :: (Integral a, Show a) => a -> String
+  fizzBuzz n
+    | n `mod` 15 == 0 = "FizzBuzz"
+    | n `mod` 3 == 0 = "Fizz"
+    | n `mod` 5 == 0 = "Buzz"
+    | otherwise     = show n
